c6b3391c4091c86cbe40d6e74ad63eaf6ff1afa9
Md Lavlu Sheikh <mdlavlusheikh200@gmail.com>
2025-11-29 15:39:37 +0600

    Update ইকরা নূরানী একাডেমি frontend - 2025-11-29 09:39:07

---
+++ pages/staff.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
+
+export default function Staff() {
+  return (
+    <Layout>
+      <Head>
+        <title>Staff - ইকরা নূরানী একাডেমি</title>
+      </Head>
+      <main className="container mx-auto px-4 py-8">
+        <h1 className="text-4xl font-bold mb-6">Our Staff</h1>
+        <p>Staff information coming soon...</p>
+      </main>
+    </Layout>
+  )
+}
