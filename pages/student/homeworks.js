458abfbb4c7a7e321bb7a90d9b90bff167ce8a60
Md Lavlu Sheikh <mdlavlusheikh200@gmail.com>
2025-11-29 15:40:14 +0600

    Update ইকরা নূরানী একাডেমি frontend - 2025-11-29 09:39:07

---
+++ pages/student/homeworks.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function StudentHomeworks() {
+  return (
+    <div className="min-h-screen p-8">
+      <Head>
+        <title>My Homeworks - ইকরা নূরানী একাডেমি</title>
+      </Head>
+      <h1 className="text-3xl font-bold mb-6">My Homeworks</h1>
+      <p>Homework assignments...</p>
+    </div>
+  )
+}
