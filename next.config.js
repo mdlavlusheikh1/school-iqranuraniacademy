c4ef29a8af6d35ed4eac4ad96903255e20bc15e1
Md Lavlu Sheikh <mdlavlusheikh200@gmail.com>
2025-11-29 15:17:52 +0600

    Deploy ইকরা নূরানী একাডেমি frontend

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    TENANT_SLUG: 'iqranuraniacademy',
+    TENANT_NAME: 'ইকরা নূরানী একাডেমি',
+  },
+}
+
+module.exports = nextConfig
